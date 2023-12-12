488cff0ce83823b1187e495176d9914bcfe32b80
marwakaabar <marwa.kaabar@esprit.tn>
2023-12-12 20:59:06 +0100

    eureka

--- src/main/java/com/example/microo/MicrooApplication.java
+++ src/main/java/com/example/microo/MicrooApplication.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicrooApplication {
 
     public static void main(String[] args) {
